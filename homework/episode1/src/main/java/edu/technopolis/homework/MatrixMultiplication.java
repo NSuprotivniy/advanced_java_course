8306b277f0e3739cf00afc294dc0001e7a147044
Nikita Suprotivniy <n.suprotivniy@gmail.com>
2016-11-11 16:24:51 +0300

    Homework by Nikita Suprotivniy

--- homework/episode1/src/main/java/edu/technopolis/homework/MatrixMultiplication.java
+++ homework/episode1/src/main/java/edu/technopolis/homework/MatrixMultiplication.java
@@ -1,12 +1,84 @@
 package edu.technopolis.homework;
 
+import javax.management.DynamicMBean;
+
 /**
  * Matrix multiplication home task.
  * <br/>
  * Matrix dimension and elements are passed as CLI arguments.
  */
-public class MatrixMultiplication {
-    public static void main(String... args) {
 
+/**
+ * Created by nsuprotivniy on 20.10.16.
+ */
+
+public class MatrixMultiplication {
+		
+		public static int[][] matrixMult(int[][] A, int[][] B) throws MatrixException, NullPointerException {
+
+        if(A == null || B == null) throw new NullPointerException("Matrix is null");
+        if (A[0].length != B.length) throw new MatrixException("Incorrect matrix size");
+
+
+        int a = A.length, b = B[0].length, m = A[0].length;
+
+        int[][] resualt = new int[a][b];
+
+        for (int i = 0; i < a; i++)
+            for (int j = 0; j < b; j++)
+                for (int k = 0; k < m; k++)
+                    resualt[i][j] += A[i][k] * B[k][j];
+
+        return resualt;
+    }
+
+
+
+    public static void main(String args[]) {
+
+        if (args.length == 0) {
+            System.out.println("Invalid input.");
+            return;
+        }
+				
+				// Read matrixes from args
+        int n = Integer.parseInt(args[0]), m = Integer.parseInt(args[1]);
+        int x = Integer.parseInt(args[2]), y = Integer.parseInt(args[3]);
+
+        int[][] A = new int[n][m];
+        int[][] B = new int[x][y];
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                A[i][j] = Integer.parseInt(args[j * n + i + 4]);
+
+        for (int i = 0; i < x; i++)
+            for (int j = 0; j < y; j++)
+                B[i][j] = Integer.parseInt(args[j * n + i + 4 + n * m]);
+
+        try {
+						// Calc result
+            int[][] resualt = matrixMult(A, B);
+
+						// Print result matrix
+            for (int i = 0; i <  resualt.length; i++) {
+                for (int j = 0; j < resualt[0].length; j++)
+                    System.out.print(resualt[i][j] + " ");
+                System.out.println();
+            }
+
+        }
+        catch (Exception excp){
+            System.out.println(excp.getMessage());
+        }
     }
 }
+
+class MatrixException extends Exception {
+
+    public MatrixException(String message) {
+        super(message);
+    }
+}
+
+    
